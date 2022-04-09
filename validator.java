e54a40707fc5fc923e6d862b95486260527cad0a
Aliasz1 <mzakwanas@protonmail.com>
2022-04-09 14:58:12 +0800

    1.0

---
+++ validator.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class validator{
+    private adding tester;
+
+    @BeforeEach
+    void setUp(){
+        tester = new adding();
+    }
+
+    @Test
+    void validateResult(){
+        
+    }
+}
